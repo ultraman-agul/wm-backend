ff5607a4c7e678b684b9153f74f5eca419f20731
ultraman-agul <agulagul@163.com>
2022-03-08 18:42:16 +0800

    feat: 根据用户位置计算距离与时间

--- controller/v1/restaurant.js
+++ controller/v1/restaurant.js
@@ -1,6 +1,6 @@
 import BaseClass from "../../prototype/baseClass.js";
 import RestaurantModel from "../../models/restaurant.js";
-
+import config from "../../config.js";
 class Restaurant extends BaseClass {
   constructor() {
     super();
@@ -8,9 +8,11 @@ class Restaurant extends BaseClass {
 
   // 获取全部餐馆
   allRestaurant = async (req, res, next) => {
+    const { lat, lng } = req.query;
     try {
       let restaurants = ""; //餐馆信息
       restaurants = await RestaurantModel.find({}, "-_id");
+      await this.getDistance(restaurants, lat, lng);
       res.send({
         status: 200,
         message: "获取全部餐馆列表成功",
@@ -24,6 +26,34 @@ class Restaurant extends BaseClass {
       });
     }
   };
+
+  //获取餐馆时计算距离以及驾车时间
+  async getDistance(restaurants, lat, lng) {
+    for (let i = 0; i < restaurants.length; i++) {
+      let result = null;
+      result = await this.fetch(
+        "https://apis.map.qq.com/ws/distance/v1/matrix",
+        {
+          mode: "driving",
+          from: `${lat},${lng}`,
+          to: `${restaurants[i].lat},${restaurants[i].lng}`,
+          key: config.tencentkey,
+        }
+      );
+
+      if (result.status !== 0) {
+        //请求出错时  设置给假数据
+        restaurants[i].distance = "10km";
+        restaurants[i].delivery_time_tip = "50分钟";
+      } else {
+        let element = result.result.rows[0].elements[0];
+        restaurants[i].distance = (element.distance / 1000).toFixed(1) + "km"; //计算距离
+        restaurants[i].delivery_time_tip =
+          (element.duration / 60).toFixed(1) + "分钟";
+      }
+    }
+    return restaurants;
+  }
 }
 
 export default new Restaurant();
