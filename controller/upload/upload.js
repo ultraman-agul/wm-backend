ec86380a7b23c38fcdd4e11bf8f5f4b5a0771ea6
ultraman-agul <agulagul@163.com>
2022-03-10 16:42:47 +0800

    feat：上传食品图片

--- controller/upload/upload.js
+++ controller/upload/upload.js
@@ -30,5 +30,28 @@ class Upload extends BaseClass {
 
     return req.pipe(busboy);
   }
+
+  // 食品图片上传
+  uploadFoodPic(req, res) {
+    const __dirname = path.resolve();
+    const busboy = new Busboy({ headers: req.headers });
+    let visitUrl;
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+      const saveTo = path.join(__dirname, "public/foods", filename);
+      // __dirname获取的是项目的根路径，而不是当前文件的路径！ 所以此处是根目录下的public/foods
+      visitUrl = "/foods/" + filename;
+      file.pipe(fs.createWriteStream(saveTo));
+    });
+
+    busboy.on("finish", function () {
+      res.send({
+        status: 200,
+        url: visitUrl,
+        message: "文件上传成功",
+      });
+    });
+
+    return req.pipe(busboy);
+  }
 }
 export default new Upload();
--- routes/upload.js
+++ routes/upload.js
@@ -3,4 +3,5 @@ import Upload from "../controller/upload/upload.js";
 
 const router = express.Router();
 router.post("/upload", Upload.upload); // 上传图片
+router.post("/uploadFoodPic", Upload.uploadFoodPic);
 export default router;
